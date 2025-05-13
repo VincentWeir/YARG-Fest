99ab44c5bae599edbd5999e34a70c80cd304ab05
Vincent Weir <87042862+VincentWeir@users.noreply.github.com>
2025-05-12 23:00:42 -0400

    icons and shi

--- Assets/Script/Menu/MusicLibrary/Sidebar.cs
+++ Assets/Script/Menu/MusicLibrary/Sidebar.cs
@@ -174,16 +174,16 @@ namespace YARG.Menu.MusicLibrary
             */
 
 
-            _difficultyRings[0].SetInfo("vocals", Instrument.Keys, entry[Instrument.Keys]);
+            _difficultyRings[0].SetInfo("keys", Instrument.Keys, entry[Instrument.Keys]);
             _difficultyRings[1].SetInfo("bass", Instrument.FiveFretBass, entry[Instrument.FiveFretBass]);
             _difficultyRings[2].SetInfo("guitar", Instrument.FiveFretGuitar, entry[Instrument.FiveFretGuitar]);
-            _difficultyRings[3].SetInfo("drums", Instrument.FiveFretRhythm, entry[Instrument.FiveFretRhythm]);
-            _difficultyRings[4].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
-            _difficultyRings[5].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
-            _difficultyRings[6].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
-            _difficultyRings[7].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
-            _difficultyRings[8].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
-            _difficultyRings[9].SetInfo("keys", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[3].SetInfo("rhythm", Instrument.FiveFretRhythm, entry[Instrument.FiveFretRhythm]);
+            _difficultyRings[4].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[5].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[6].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[7].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[8].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
+            _difficultyRings[9].SetInfo("vocals", Instrument.ProGuitar_17Fret, entry[Instrument.ProGuitar_17Fret]);
         }
 
         public void PrimaryButtonClick()
